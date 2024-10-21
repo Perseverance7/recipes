964b92c84abad0babed596f1b1486a68fcc74feb
anton <eagoker@mail.ru>
2024-10-20 21:23:13 -0400

    add JWT middleware

--- pkg/service/auth.go
+++ pkg/service/auth.go
@@ -2,14 +2,25 @@ package service
 
 import (
 	"crypto/sha1"
+	"errors"
 	"fmt"
+	"time"
 
 	"github.com/Eagoker/todo-list"
 	"github.com/Eagoker/todo-list/pkg/repository"
+	"github.com/dgrijalva/jwt-go"
 )
 
-const salt = "etdgfww12grgdsfedf"
+const (
+	salt = "etdgfww12grgdsfedf"
+	signingKey = "efsvcetdfvcdfdgbrdfbvdvvf"
+	tokenTTL = 12 *time.Hour
+)
 
+type TokenClaims struct {
+	jwt.StandardClaims
+	UserId int `json:"user_id"`
+}
 type AuthService struct {
 	repo repository.Authorization
 }
@@ -21,6 +32,43 @@ func NewAuthService(repo repository.Authorization) *AuthService {
 func (s *AuthService) CreateUser(user todo.User) (int, error) {
 	user.Password = generatePasswordHash(user.Password)
 	return s.repo.CreateUser(user)
+}
+
+func (s *AuthService) GenerateToken(username, password string) (string, error){
+	user, err := s.repo.GetUser(username, generatePasswordHash(password))
+	if err != nil {
+		return "", err
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, &TokenClaims{
+		jwt.StandardClaims{
+			ExpiresAt: time.Now().Add(tokenTTL).Unix(),
+			IssuedAt: time.Now().Unix(),
+		},
+		user.Id,
+	})
+	return token.SignedString([]byte(signingKey))
+}
+
+func(s *AuthService) ParseToken(accessToken string) (int, error) {
+	token, err := jwt.ParseWithClaims(accessToken, &TokenClaims{}, func(token *jwt.Token) (interface{}, error){
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, errors.New("invalid signing method")
+		}
+
+		return []byte(signingKey), nil
+	})
+
+	if err != nil {
+		return 0, err
+	}
+
+	claims, ok := token.Claims.(*TokenClaims)
+	if !ok {
+		return 0, errors.New("token claims are not of type *tokenClaims")
+	}
+
+	return claims.UserId, nil
 }
 
 func generatePasswordHash(password string) string {
