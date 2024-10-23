bf68496068aa1bb0d032b0e909f68bab28b85936
anton <eagoker@mail.ru>
2024-10-23 16:01:21 -0400

    create doc with swagger

--- pkg/repository/todo_item_postgres.go
+++ pkg/repository/todo_item_postgres.go
@@ -66,7 +66,7 @@ func (r *TodoItemPostgres) GetById(userId, itemId int) (todo.TodoItem, error) {
 }
 
 func (r *TodoItemPostgres) Delete(userId, itemId int) error {
-	query := fmt.Sprintf("DELETE FROM %s ti USING %s li, %s ul WHERE ti.id = li.item_id AND li.list.id = ul.list_id AND ul.user_id = $1 AND ti.id = $2",
+	query := fmt.Sprintf("DELETE FROM %s ti USING %s li, %s ul WHERE ti.id = li.item_id AND li.list_id = ul.list_id AND ul.user_id = $1 AND ti.id = $2",
 		todoItemsTable, listsItemsTable, usersListsTable)
 	_, err := r.db.Exec(query, userId, itemId)
 	return err
@@ -99,7 +99,7 @@ func (r *TodoItemPostgres) Update(userId, itemId int, input todo.UpdateItemInput
     setQuery := strings.Join (setValues, ", ")
 
     query := fmt.Sprintf(
-        "UPDATE %s ti SET %s FROM %s li, %s ul WHERE ti.id = ti.item_id AND li.list_id = ul.list_id AND il.user_id = $%d AND ti.id = $%d",
+        "UPDATE %s ti SET %s FROM %s li, %s ul WHERE ti.id = li.item_id AND li.list_id = ul.list_id AND ul.user_id = $%d AND ti.id = $%d",
         todoItemsTable, setQuery, listsItemsTable, usersListsTable ,argId, argId+1)
 
     args = append(args, userId, itemId)
